ae9a3f72077be7e314756bf058f5a9f025ff453b
S01049558 <S01049558@acad.tri-c.edu>
2015-02-04 10:43:17 -0800

    Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student A = new Student();
+            A.lName = "Modou";
+            A.fName = "Jallow";
+            A.Age = 38;
+            A.EmailAddress = "s01020445@acad.tri-.edu";
+            A.socialSecurityNumber = 218986543;
+
+            Student B = new Student();
+            B.lName = "Douo";
+            B.fName = "Germain";
+            B.Age = 37;
+            B.EmailAddress = "S01049558@acad.tri-c.edu";
+            B.socialSecurityNumber = 217432277;
+
+            Console.WriteLine(A.lName + ", " + A.fName + ", " + A.Age + A.EmailAddress + ", " + A.socialSecurityNumber);
+            Console.WriteLine(B.lName + ", " + B.fName + ", " + B.Age + B.EmailAddress + ", " + B.socialSecurityNumber);
+            Console.ReadLine();
+
+            
+          
+        }  
+    }
+}
---
+++ Lab2/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab2
+{
+    public class Student
+    {
+        public string fName;
+        public string lName;
+        public int Age;
+        public string EmailAddress;
+        public int socialSecurityNumber;
+    }
+}
